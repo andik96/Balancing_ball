3d855c273f0bcb46b8afaab4f64c9660d0aba36f
Andreas Kemperle <andreas.kemperle@edu.fh-joanneum.at>
2018-06-11 21:29:33 +0200

    First try for PID optimizer implementation

---
+++ BalanceLib/Pid_optimizer.cpp
@@ -0,0 +1,37 @@
+
+/********************************
+#   Balancing Ball              #
+#   Pid_optimizer.cpp           #
+#                               #
+#   Authors:                    #
+#       KEMPERLE Andreas        #
+#       NEUHOLD  Michaela       #
+#       SOUKUP   Jakob          #
+#       WINKLER  Andreas        #
+#                               #
+#   created: 2018/06/11         #
+#   Version: 2018/06/11 - V1.0  #
+********************************/
+
+
+// ===============================================================
+// INCLUDES
+
+#include "Pid_optimizer.h"
+
+
+// #################################### SECTION BREAK ####################################
+
+
+// ===============================================================
+// RUN OPTIMIZER
+
+void Pid_optimizer::run(Pid_controller& my_controller)
+{
+	// TODO: PUT OPTIMIZER CODE HERE
+}
+
+
+//struct Pid Pid_optimizer::get_optimum()
+//{
+//}
---
+++ BalanceLib/Pid_optimizer.h
@@ -0,0 +1,51 @@
+
+/********************************
+#   Balancing Ball              #
+#   Pid_optimizer.h             #
+#                               #
+#   Authors:                    #
+#       KEMPERLE Andreas        #
+#       NEUHOLD  Michaela       #
+#       SOUKUP   Jakob          #
+#       WINKLER  Andreas        #
+#                               #
+#   created: 2018/06/11         #
+#   Version: 2018/06/11 - V1.0  #
+********************************/
+
+#pragma once
+
+// ===============================================================
+// INCLUDES
+
+#include "Pid_controller.h"
+
+
+// ===============================================================
+// PID STRUCT
+
+struct Pid
+{
+	double kp_ = 1;
+	double ki_ = 0;
+	double kd_ = 0;
+};
+
+
+// #################################### SECTION BREAK ####################################
+
+
+// ===============================================================
+// PID OPTIMIZER CLASS
+
+class Pid_optimizer
+{
+public:
+	void run(Pid_controller& my_controller);
+	// struct Pid get_optimum();
+
+private:
+	// Pid pid_opt_data_;
+	Pid_controller& my_controller;
+};
+
